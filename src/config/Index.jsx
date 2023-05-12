22981e724af1529685e1b1827a9a0d055de651f5
William <anazawilliam1@gmail.com>
2023-05-12 17:04:22 +0100

    deployed to netlify

---
+++ src/config/Index.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArfKEQwieHfVhmMkPxGjJ5mh9zCgY2U3E",
+  authDomain: "auth-692a8.firebaseapp.com",
+  projectId: "auth-692a8",
+  storageBucket: "auth-692a8.appspot.com",
+  messagingSenderId: "279295002718",
+  appId: "1:279295002718:web:0c49a473a4140743ec0ccf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
